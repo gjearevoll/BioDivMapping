1113468d2f3ad3af593c67b1479036c8b68db94e
samaperrin <sam.perrin@ntnu.no>
2023-06-29 16:15:46 +0200

    Added annotation to speciesDataProcessing

--- pipeline/processing/speciesDataProcessing.R
+++ pipeline/processing/speciesDataProcessing.R
@@ -12,6 +12,7 @@ if (!exists("dateAccessed")) {
 
 # Import species list
 focalSpecies <- read.csv("data/external/focalSpecies.csv", header = T)
+focalSpecies <- focalSpecies[focalSpecies$selected,]
 focalGroups <- unique(focalSpecies$taxonomicGroup)
 
 # Import datasets
@@ -21,21 +22,31 @@ speciesDataList <- readRDS(paste0(tempFolderName, "/speciesDataImported.RDS"))
 speciesData <- speciesDataList[["species"]]
 metadata <- speciesDataList$metadata$metadata
 
-# Loop through and find PA datasets
+# Loop through and apply different processing scripts to various data types based on rules
+# For now we only have three rules
+# 1. Is it presence/absence? (but not ANO data)
+# 2. Is it ANO Data?
+# 3. Is it presence only?
+
+# Start a processed data list
 processedData <- list()
 namesProcessedData <- c()
 for (ds in 1:length(speciesData)) {
   focalData <- speciesData[[ds]]
   
+  # If the dataset is empty, skip it
   if (nrow(focalData) == 0) next
   
   dataType <- unique(focalData$dataType)
   datasetName <- names(speciesData)[ds]
-  if (dataType == "PA" & datasetName != "ANOData") {
+  if (dataType == "PA" & datasetName != "ANOData") { 
+    # Here we apply our conversion script for presence/absence data
     source("utils/presenceAbsenceConversion.R")
   } else if (datasetName == "ANOData") {
+    # ANO data has a different nedpoint and has already been taken care of in the utils/ANOIntegration scropt
     newDataset <- focalData[,c("simpleScientificName", "geometry", "individualCount", "dataType")]
   } else {
+    # No need to do anything to presence only data (yet)
     newDataset <- focalData[,c("simpleScientificName", "geometry", "dataType")]
   }
   processedData[[ds]] <- newDataset
@@ -43,5 +54,6 @@ for (ds in 1:length(speciesData)) {
 }
 names(processedData) <- namesProcessedData
 
+# Save for use in model construction
 processedData <- processedData[lapply(processedData,length)>0]
 saveRDS(processedData, paste0(folderName, "/temp/speciesDataProcessed.RDS"))

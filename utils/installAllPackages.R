b99d5797abbd59e678f160eae6f1389db91e72e6
Sam Wenaas Perrin <samp@wallace.win.ntnu.no>
2023-08-18 08:04:09 +0000

    Error regarding PointedSDMs/intSDM fixed

--- utils/installAllPackages.R
+++ utils/installAllPackages.R
@@ -8,12 +8,12 @@ if(!("INLA" %in% row.names(installedPackages))) {
   install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
   }
 if(!("intSDM" %in% row.names(installedPackages))) {
-  devtools::install_github("PhilipMostert/PointedSDMs") 
+  devtools::install_github("PhilipMostert/intSDM") 
 }
 
 # Now we bring int he rest of the necessary packages
 necessaryPackages <- c("rgbif", "sf", "stringr", "dplyr", "rinat", "raster", "csmaps", "ggplot2",
-                       "shiny", "shinydashboard",
+                       "shiny", "shinydashboard", "PointedSDMs",
                        "shinyjs", "inlabru", "randomcoloR", "plotKML")
 uninstalledPackages <- necessaryPackages[!(necessaryPackages %in% row.names(installedPackages))]
 install.packages(uninstalledPackages)

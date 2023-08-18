9633071857a9c70c156a59a352f74663d01d04a7
Philip Mostert <philip.s.mostert@ntnu.no>
2023-08-18 10:27:03 +0200

    Update meshTest.R
    Changes to mesh.utils using INLA rather than intSDM

--- utils/meshTest.R
+++ utils/meshTest.R
@@ -4,11 +4,12 @@
 
 # THis file creates and visualises a very quick INLA Mesh
 
-workflow <- startWorkflow(
-  Projection = '+proj=utm +zone=32 +ellps=WGS84 +datum=WGS84 +units=m +no_defs',
-  Species = "none",
-  saveOptions = list(projectDirectory = "data", projectName =  "test"), Save = FALSE
-)
-workflow$addArea(Object = st_sf(regionGeometry), resolution = '60')
-workflow$addMesh(cutoff= myMesh$cutoff, max.edge=myMesh$max.edge, offset= myMesh$offset)
-print(workflow$plot(Mesh = TRUE))
+mesh <- INLA::inla.mesh.2d(inlabru::fm_as_inla_mesh_segment(regionGeometry),
+                           crs = inlabru::fm_crs('+proj=utm +zone=32 +ellps=WGS84 +datum=WGS84 +units=m +no_defs'),
+                           cutoff= myMesh$cutoff, 
+                           max.edge=myMesh$max.edge, 
+                           offset= myMesh$offset)
+
+ggplot2::ggplot() +
+  ggplot2::geom_sf(data = sf::st_boundary(private$Area)) +
+  inlabru::gg(mesh)

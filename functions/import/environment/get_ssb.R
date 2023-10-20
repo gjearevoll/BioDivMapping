ebfd156e2c1d0cec68307c68d4b054b90170a658
Sam Wenaas Perrin <samp@wallace.win.ntnu.no>
2023-10-20 10:44:22 +0000

    Simplified script and changed endpoint links

--- functions/import/environment/get_ssb.R
+++ functions/import/environment/get_ssb.R
@@ -1,41 +1,23 @@
 # function derives values found on this page: https://www.ssb.no/natur-og-miljo/geodata and 
 # at https://kartkatalog.geonorge.no/metadata/befolkning-paa-rutenett-1000m-2016-2019-wfs/20d06bca-484c-4353-a24b-61046296f5da
 
-get_ssb <- function(focalParameter, rutenett = NA, getRutenett = TRUE, resolution = "1000") {
+get_ssb <- function(focalParameter, resolution = "1000") {
   
   # Define endpoints for different resolutions and variables
-  resolutionFileCheck <- data.frame(human_density = c("https://www.ssb.no/natur-og-miljo/_attachment/389479?_ts=16b45e39840",
-                                                      "https://www.ssb.no/natur-og-miljo/_attachment/389480?_ts=16b45e3cef0"),
-                                    building_density = c("https://www.ssb.no/natur-og-miljo/_attachment/461715?_ts=17edde4d2e0",
-                                                         "https://www.ssb.no/natur-og-miljo/_attachment/461716?_ts=17edde501c0"),
+  resolutionFileCheck <- data.frame(human_density = c("http://wfs.geonorge.no/skwms1/wfs.befolkningsstatistikkrutenett250mhistorisk?service=wfs&Version=2.0.0&request=GetCapabilities",
+                                                      "https://wfs.geonorge.no/skwms1/wfs.befolkningsstatistikkrutenett1kmhistorisk?service=WFS&Version=2.0.0&request=GetCapabilities"),
+                                    building_density = c("https://wfs.geonorge.no/skwms1/wfs.bygningsstatistikkrutenett250m?service=WFS&request=GetCapabilities",
+                                                         "https://wfs.geonorge.no/skwms1/wfs.bygningsstatistikkrutenett1000m?service=WFS&Version=2.0.0&request=GetCapabilities"),
                                     row.names = c("250", "1000"))
   focalEndpoint <- resolutionFileCheck[resolution, focalParameter]
   
-  # Check if rutenett is provided, if not, download from SSB and save locally
-  if (getRutenett == TRUE) { 
-    download.file("https://www.ssb.no/natur-og-miljo/_attachment/375076?_ts=1685c0b8770", 
-                  destfile = "data/temp/ssb/rutenett.zip", mode = "wb")
-    fileNames <- unzip("data/temp/ssb/rutenett.zip", exdir = "data/temp/ssb", list = TRUE)
-    shpFile <- fileNames$Name[grepl(".shp", fileNames$Name) & !grepl(".shp.xml", fileNames$Name)]
-    unzip("data/temp/ssb/rutenett.zip", exdir = "data/temp/ssb")
-    rutenett <- read_sf(paste0("data/temp/ssb/",shpFile))[,c("geometry", "SSBid")]
-    saveRDS(rutenett, "data/temp/ssb/rutenett.RDS")
-  }
+  V0 <- vect(paste0("WFS:", focalEndpoint))
   
-  # Download csv file for relevant variable  with values
-  download.file(focalEndpoint, destfile = paste0(tempFolderName, focalParameter, ".zip"), mode= "wb")
-  fileName <- unzip(paste0(tempFolderName, focalParameter, ".zip"), list = TRUE)
-  unzip(paste0(tempFolderName, focalParameter, ".zip"), exdir = paste0(tempFolderName, "ssb"))
-  values <- read.csv(paste0(tempFolderName, "ssb/", fileName$Name), sep = ";") %>%
-    dplyr::select(1,2)
-  colnames(values) <- c("SSBId", "value")
+  # Project onto our rasters
+  R0 <- rast("data/external/environmentalCovariates/aspect.tiff")
+  R2 <- terra::rasterize(project(V0,R0), R0, field = "bui0all")
+  R3 <- lapp(R2, function(x) ifelse(is.na(x), 0, x))
   
-  # Match to rutenett
-  rutenett$value <- values$value[match(rutenett$SSBid, values$SSBId)]
-  rutenett$value[is.na(rutenett$value)] <- 0
-  
-  rasterSample <- raster("data/external/environmentalCovariates/aspect.tiff")
-  rasterizedVersion <- rast(fasterize(sf = rutenett, raster = rasterSample, field = "value"))
-  return(rasterizedVersion)
+  return(R3)
   
 }

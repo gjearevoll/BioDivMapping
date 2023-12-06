14e5bec3c794fea740a1ee982c707585da86ffbb
Sam Wenaas Perrin <samp@wallace.win.ntnu.no>
2023-12-06 11:45:17 +0000

    Updated results compilation to process sampling bias too

--- pipeline/models/utils/modelResultsCompilation.R
+++ pipeline/models/utils/modelResultsCompilation.R
@@ -27,9 +27,13 @@ for (i in seq_along(focalTaxaRun)) {
   
   # Extract individual species intensities
   predictions <- list.files(groupFolderLocation, 
-                            pattern = ".rds", 
+                            pattern = "Predictions.rds", 
+                            full.names = T, recursive = T)
+  biases <- list.files(groupFolderLocation, 
+                            pattern = "biasPreds.rds", 
                             full.names = T, recursive = T)
   speciesIntensities <- lapply(predictions, readRDS)
+  taxaBias <- lapply(biases, readRDS)
   
   # Scale all columns for each species between 0 and 1
   speciesIntensitiesScaled <- lapply(1:length(speciesIntensities), FUN = function(x) {
@@ -41,9 +45,21 @@ for (i in seq_along(focalTaxaRun)) {
     reprojectedIntensity
   })
   
+  # Get all biases for datasets
+  taxaBiasesScaled <- lapply(1:length(taxaBias), FUN = function(x) {
+    biasList <- taxaBias[[x]]
+    biasVector <- biasList$biasFields$sharedBias$mean
+    biasScaled <- (biasVector - min(biasVector))/(max(biasVector)-min(biasVector))
+    biasList$biasFields$sharedBias$mean <- biasScaled
+    reprojectedBias <- suppressMessages(st_transform(biasList$biasFields$sharedBias, "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+    reprojectedBias
+  })
+  biasNames <- gsub("/biasPreds.rds", "", list.files(groupFolderLocation, pattern = "biasPreds.rds", recursive = T))
+  
   # Name columns and rescale aggregate for biodiversity metric
   names(speciesIntensitiesScaled) <- speciesRun
-  outputList[[focalGroup]] <- speciesIntensitiesScaled
+  names(taxaBiasesScaled) <- gsub("_", " ", biasNames)
+  outputList[[focalGroup]] <- list(speciesIntensities = speciesIntensitiesScaled, bias = taxaBiasesScaled)
   
   print(paste0("Finished running ", focalGroup))
   

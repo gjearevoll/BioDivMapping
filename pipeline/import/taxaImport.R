8380c0756578f0bbfb3c1f477c267436ee738186
Ron Togunov <20913113+RRTogunov@users.noreply.github.com>
2023-12-07 16:39:43 +0100

    save focalTaxa.csv after adding missing keys

--- pipeline/import/taxaImport.R
+++ pipeline/import/taxaImport.R
@@ -42,8 +42,6 @@ if(file.exists(paste0(folderName, "/focalTaxa.csv"))){
   focalTaxon <- read.csv(paste0(folderName, "/focalTaxa.csv"), header = T)
 } else {
   focalTaxon <- read.csv("data/external/focalTaxa.csv", header = T)
-  # save for reference
-  write.csv(focalTaxon, paste0(folderName, "/focalTaxa.csv"), row.names = FALSE)
 }
 
 # Refine focal taxon
@@ -54,6 +52,8 @@ missingKey <- is.na(focalTaxon$key) & focalTaxon$level != "polyphyla"
 focalTaxon$key[missingKey] <- getUsageKeys(focalTaxon$scientificName[missingKey], 
                                            rank = focalTaxon$level[missingKey], 
                                            strict = T)
+# save for reference
+write.csv(focalTaxon, paste0(folderName, "/focalTaxa.csv"), row.names = FALSE)
 
 # Introduce polyphyletic groups
 polyphyleticSpecies <- read.csv("data/external/polyphyleticSpecies.csv") %>%

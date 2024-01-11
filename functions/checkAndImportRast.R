25d15b26bf6a7b11d3cd14846918a01076e37fde
Ron Togunov <20913113+RRTogunov@users.noreply.github.com>
2024-01-11 17:07:06 +0100

    Create checkAndImportRast.R
    function will explore given dataPath for raster of a given parameter that encompasses a regionGeometry of interest

---
+++ functions/checkAndImportRast.R
@@ -0,0 +1,20 @@
+# Define the path for data source
+checkAndImportRast <- function(parameter, regionGeometry, dataPath, fileType = "tiff") {
+  # Initialize the raster found flag
+  raster <- NULL
+  
+  # List all files in the directory that match the parameter
+  filePattern <- paste0(parameter, sprintf( "_.*\\.%s$", fileType))
+  fileList <- list.files(path = dataPath, pattern = filePattern, full.names = TRUE)
+  
+  # Check each file
+  for (file in fileList) {
+    rastObj <- terra::rast(file)
+    if (isSubset(regionGeometry, rastObj)) {
+      raster <- rastObj
+      cat(sprintf("Raster for '%s' encompassing region found and imported successfully!\n", parameter))
+      break
+    } 
+  } 
+  return(raster)
+}

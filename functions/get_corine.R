b3a99a295874aeb264da343ad7477461a42d3842
Sam Wenaas Perrin <samp@wallace.win.ntnu.no>
2024-02-02 14:28:30 +0000

    Addition of reclassification option to CORINE

--- functions/get_corine.R
+++ functions/get_corine.R
@@ -63,8 +63,19 @@ get_corine <- function(zip_path = NA, output_path = NA) {
     levels(corine)[[1]][,2] <- gsub(",", "", levels(corine)[[1]][,2])
   }
   
+  if(!file.exists("data/temp/corine/corineReclassification.csv")) {
+    warning("No CORINE reclassification system has been provided. Using all 52 potential categories.")
+  } else {
+    corineReclassification <- read.csv("data/temp/corine/corineReclassification.csv", header = TRUE)
+    reclassTable <- levels(corine)[[1]]
+    reclassTable$newLabel <- corineReclassification$newCategory[match(reclassTable$label, corineReclassification$corineCategory)]
+    reclassTable <- reclassTable[,c("value", "newLabel")] %>% rename(label = newLabel)
+    corine <- addCats(corine, reclassTable, merge = TRUE)
+  }
+  
+  
   if(!is.na(output_path)) {
-    writeRaster(corine, file.path(output_path, "corine.tif"))
+    writeRaster(newRaster, file.path(output_path, "corine.tif"))
   }
   
   out <- corine

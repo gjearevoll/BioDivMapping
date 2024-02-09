1a33539ad9e53470bcb5ca5be01dcdfe280dd526
Sam Wenaas Perrin <samp@wallace.win.ntnu.no>
2024-02-09 03:52:08 +0000

    MODIS packages added to installation

--- pipeline/installAllPackages.R
+++ pipeline/installAllPackages.R
@@ -14,6 +14,7 @@ if(!("intSDM" %in% row.names(installedPackages))) {
 # Now we bring int he rest of the necessary packages
 necessaryPackages <- c("rgbif", "sf", "stringr", "dplyr", "rinat", "raster", "csmaps", "ggplot2",
                        "shiny", "shinydashboard", "PointedSDMs", "terra",
-                       "shinyjs", "inlabru", "randomcoloR", "plotKML", "fasterize", "httr", "jsonlite", "tidyterra", "rasterdiv")
+                       "shinyjs", "inlabru", "randomcoloR", "plotKML", "fasterize", "httr", "jsonlite",
+                       "tidyterra", "rasterdiv", "MODISTools", "MODIStsp")
 uninstalledPackages <- necessaryPackages[!(necessaryPackages %in% row.names(installedPackages))]
 install.packages(uninstalledPackages)

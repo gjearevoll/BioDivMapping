4628659205495ca4cac640be2bb1c07804abc5bd
Sam Wenaas Perrin <samp@wallace.win.ntnu.no>
2026-01-28 12:40:16 +0000

    Add options for bias fields, PO/year trends

--- pipeline/temporalRun/temporalScriptRun.R
+++ pipeline/temporalRun/temporalScriptRun.R
@@ -5,25 +5,35 @@ library(terra)
 library(inlabru)
 library(sf)
 library(INLA)
+library(dplyr)
+library(qs)
 ##1.1 Data needs to be a list of sf datasets,
 
 args <- commandArgs(trailingOnly = TRUE)
 
 start <- Sys.time()
 
-i <- as.numeric(args[1])
-dateAccessed <- args[2]
+i <- 1 #as.numeric(args[1])
+dateAccessed <- "2025-12-11" #args[2]
+modelSave <- TRUE #args[3]
+biasField <- TRUE #args[4]
+POTrend <- FALSE #args[5]
+yearTrend <- TRUE #args[6]
+
+# Define model characteristics
+modelRun <- c("biasField", "POTrend", "yearTrend")[c(biasField, POTrend, yearTrend)]
+saveRDS(modelRun, paste0(modelFolder, "/modelRun.RDS"))
 
 ##Load in data here
 
-#dateAccessed <- "2025-10-20"
+#dateAccessed <- "2025-12-11"
 folderName <- paste0("data/run_", dateAccessed)
 readRDS(paste0(folderName,"/controlPars.RDS")) %>% 
   list2env(envir = .GlobalEnv)
 sapply(list.files("functions", full.names = TRUE), source)
 
 cat("Loading species data \n")
-specData <- readRDS(paste0(folderName, "/speciesDataProcessed.RDS"))
+specData <- qread(paste0(folderName, "/speciesDataProcessed.qs"))
 
 #Select species
 segmentList <- readRDS(paste0(folderName, "/segmentList.RDS"))
@@ -35,7 +45,7 @@ specData3 <- specData2[(lapply(specData2, nrow) != 0)]
 
 # Create species folder
 cat("Creating model output folder \n")
-modelFolder <- paste0(folderName, "/modelOutputs/", speciesSelect)
+modelFolder <- paste0(folderName, "/modelOutputsBiasYearTrend/", speciesSelect)
 if (!dir.exists(modelFolder)) {
   dir.create(modelFolder)
 }
@@ -46,6 +56,9 @@ PODatasets <- unlist(lapply(specData3, FUN = function(x) {
 }))
 presenceOnlyData <- do.call(rbind, specData3[PODatasets])
 specData3 <- c(list(presenceOnlyData = presenceOnlyData), specData3[!PODatasets])
+
+# Get rid of empty datasets
+specData3 <- specData3[unlist(lapply(specData3, FUN = function (x) {!is.null(x)}))]
 
 # Change names
 cat("Changing species data names 1\n")
@@ -69,8 +82,7 @@ names(specData3) <- gsub(" ", '', names(specData3))
 ##Load in  covariates here
 
 cat("Loading environmental data \n")
-#envData <- lapply(readRDS(paste0(folderName, "/environmentalDataImported.RDS")), terra::unwrap)
-envData <- lapply(readRDS(paste0(folderName, "/environmentalDataImportedAll.RDS")), terra::unwrap)
+envData <- lapply(readRDS(paste0(folderName, "/temp/environmentalDataImported.RDS")), terra::unwrap)
 
 ##Make sure lyr names == cov names
 ##Filter all years not in occ_data to ensure everything works fine
@@ -80,7 +92,7 @@ envData <- mapply(function(x, y) {
   
   if (terra::nlyr(x) == 1) names(x) <- y
   
-  if (terra::nlyr(x) > 4) x[[c('2000', '2006', '2012', '2018')]] # or however
+  if (terra::nlyr(x) > 4) x[[as.character(yearInterval)]] # or however
   else x
   
 }, x = envData, y = names(envData))
@@ -135,33 +147,41 @@ model_setup <- startISDM(specDataProj, ##list of dataset names
                          spatialCovariates = envDataProj, #Raster layers as a list
                          responsePA = 'individualCount',
                          temporalName = 'year',
-                         Formulas = list(biasFormula = ~ distance_roads))
+                         Formulas = list(biasFormula = ~ distance_roads)
+)
 
 
 
-##1.4 Add year trend into the model
-cat("Adding year trend to model \n")
-model_setup$changeComponents('yearTrend(main = year, model = "ar1", constr = FALSE, 
-                             hyper = list(prec = list(prior = "gaussian", param = c(0, 1)), 
+##1.4 Add trends into the model
+if (yearTrend) {
+  cat("Adding year trend to model \n")
+  model_setup$changeComponents('yearTrend(main = year, model = "ar1", constr = FALSE,
+                             hyper = list(prec = list(prior = "gaussian", param = c(0, 1)),
                              rho = list(prior ="pc.cor1", param = c(0,0.9))))')
-model_setup$updateFormula(processLevel = TRUE, Formula = ~ . + yearTrend)
+  model_setup$updateFormula(processLevel = TRUE, Formula = ~ . + yearTrend)
+}
 
+if (POTrend) {
+  cat("Adding POtrend to model \n")
+  model_setup$changeComponents('POTrend(main = year, model = "ar1", hyper = list(prec = list(prior = "gaussian", param = c(0, 1))))')
+  model_setup$updateFormula(datasetName = 'presenceOnlyData',Formula = ~ . + POTrend)
+}
 
-model_setup$changeComponents('POTrend(main = year, model = "ar1", hyper = list(prec = list(prior = "gaussian", param = c(0, 1))))')
-model_setup$updateFormula(datasetName = 'TOVData',Formula = ~ . + POTrend)
+model_setup$updateFormula(datasetName = 'TOVData',Formula = ~ . - distance_roads)
 
 ##1.5 Add Bias
 #Not sure how we will do this
 #Can copy out until we figure it out
-cat("Adding bias to model \n")
-model_setup$addBias(allPO = TRUE, 
-                    shareModel = TRUE, 
-                    copyModel = FALSE, 
-                    temporalModel = list(model = "iid"))
+if (biasField) {
+  cat("Adding bias to model \n")
+  model_setup$addBias(allPO = TRUE,
+                      shareModel = TRUE,
+                      copyModel = FALSE,
+                      temporalModel = list(model = "iid"))
+}
 
 ##1.6 Add priors
 #Fixed effects
-
 cat("Adding priors to model \n")
 for (dataset in names(specDataProj)) model_setup$priorsFixed(Effect = 'Intercept',
                                                              mean.linear = 0,
@@ -172,12 +192,15 @@ for (dataset in names(specDataProj)) model_setup$priorsFixed(Effect = 'Intercept
 #Needs to change
 cat("Adding spatial and spatiotemporal priors to model \n")
 model_setup$specifySpatial(sharedSpatial = TRUE, prior.range = c(100, 0.1), prior.sigma = c(1, 0.1))
-model_setup$specifySpatial(Bias = TRUE, prior.range = c(10, 0.1), prior.sigma = c(1, 0.1))
+
+if (biasField) {
+  model_setup$specifySpatial(Bias = TRUE, prior.range = c(10, 0.1), prior.sigma = c(1, 0.1))
+}
 
 model_setup$specifyRandom(temporalModel = list(model = 'ar1', hyper = list(rho = list(prior = 'pc.cor1', param = c(0, 0.9)))))
 
-##1.7 Add spatially constant field
-#Might be worth doing if rho -> 1
+##1.7 Tightening of priors
+INLA:::inla.set.control.fixed.default(prec = 0, mean.linear = 0, prec.linear = 1) # THIS LINE TIGHTENS PRIORS FOR COVARIATE EFFECTS INLCUDING DISTANCE TO ROAD
 
 ## 2.0 Estimate models 
 
@@ -187,7 +210,7 @@ model_setup$specifyRandom(temporalModel = list(model = 'ar1', hyper = list(rho =
 cat("Fitting model \n")
 model <- fitISDM(model_setup, options = list(bru_max_iter = 1, 
                                              num.threads = 20,
-                                             verbose = TRUE,
+                                             verbose = FALSE,
                                              safe = TRUE,
                                              inla.mode = 'experimental',
                                              control.inla = list(
@@ -200,11 +223,18 @@ model <- fitISDM(model_setup, options = list(bru_max_iter = 1,
                                                stupid.search.max.iter = 50,
                                                strategy = "adaptive",
                                                diagonal = 0)))
+writeLines(model$logfile, paste0(modelFolder, '/file.txt'))
 
-##Save model
-#saveRDS(model, paste0(folderName, "/modelOutputs/temporalModel_", speciesSelect, ".RDS"))
+#Save model
+if (modelSave) {
+  qsave(model, paste0(folderName, "/modelOutputsBiasYearTrend/temporalModel_", speciesSelect, ".qs"))
+}
 
-#stop("Model run finished here")
+cat("Calculating model run time taken \n")
+end <- Sys.time()
+timeTaken <- end - start
+print(timeTaken)
+saveRDS(20 * timeTaken, paste0(modelFolder, "/timeTaken.RDS"))
 
 ##Extract everything relevant from the model
 #Save these
@@ -234,14 +264,14 @@ yearsIn <- yearInterval
 
 cat("Loading prediction data \n")
 
-predData <- readRDS(paste0(folderName, "/temp/predData.RDS"))
+predData <- qread(paste0(folderName, "/temp/predData.qs"))
 coords = st_coordinates(predData[[1]])
 
 predDataNonBias <- lapply(predData, FUN = function(x) {
-  x[,!(colnames(x) %in% "distance_roads")] 
+  x[,!(colnames(x) %in% c("distance_roads"))] 
 })
 predDataBias <- lapply(predData, FUN = function(x) {
-  x[,(colnames(x) %in% "distance_roads")] 
+  x[,(colnames(x) %in% c("distance_roads"))] 
 })
 
 
@@ -268,52 +298,8 @@ for (i in 1:length(predData)) {
 saveRDS(lapply(FixedList, terra::wrap), paste0(modelFolder, "/likelihoods.RDS"))
 
 
-# save.image(paste0(folderName, "/workspaces/derivedProducts.RData"))
-# 
-#            stop()
 
 modelMesh <- model$bru_info$model$effects$shared_spatial$main$mapper$mesh
-
-cat("Creating bias fields \n")
-##Create maps of bias fields
-BiasDat <- split(RandomComponents$sharedBias_biasField,
-                 cut(seq_along(RandomComponents$sharedBias_biasField$mean),
-                     length(yearsIn),
-                     labels = FALSE))
-
-BiasRasterMedian <- terra::rast()
-BiasRasterSD <- terra::rast()
-
-#pixelData <- fm_pixels(mesh = meshToUse, mask = regionGeometryProj)
-for (i in seq(1, length(yearsIn))) {
-  
-  BiasField <- fm_evaluate(modelMesh, data.frame(BiasDat[[i]]), loc = predData[[1]])
-  temp1 <- cbind(coords, data.frame(BiasField[, c('sd' , 'X0.5quant')]))
-  temp2 <- cbind(coords, data.frame(as.matrix(st_drop_geometry(predDataBias[[i]])) %*% 
-                                      as.matrix(RandomComponents$Bias__Effects__Comps[,c('sd', '0.5quant')])))
-  
-  names(temp1)[names(temp1) == 'X0.5quant'] <- 'median'
-  names(temp2)[names(temp2) == 'X0.5quant'] <- 'median'
-  
-  temp <- temp1
-  temp$median <- temp1$median + temp2$median
-  temp$sd <- temp1$sd + temp2$sd
-  
-  temp <- rast(temp)
-  
-  crs(temp) <- fm_wkt(meshToUse)
-  
-  names(temp) <- rep(as.character(yearsIn[i]), 2)
-  temp <- mask(temp, st_sf(regionGeometryProj))
-  BiasRasterMedian <- c(BiasRasterMedian, temp[[2]])
-  BiasRasterSD <-  c(BiasRasterSD, temp[[1]])
-  
-}
-BiasRasters <- list(BiasRasterMedian = BiasRasterMedian,
-                    BiasRasterSD = BiasRasterSD)
-saveRDS(lapply(BiasRasters, terra::wrap), paste0(modelFolder, "/biasFields.RDS"))
-
-##Save biasRaster
 
 ##Create map of spatial field
 
@@ -338,8 +324,20 @@ for (i in seq(1, length(yearsIn))) {
   crs(temp) <- fm_wkt(meshToUse)
   temp <-  mask(temp, st_sf(regionGeometryProj))
   
-  median <- temp$median + RandomComponents$yearTrend[['0.5quant']][i] + FixedList[[i]]$median + FixedComponents[paste0(repDataSet,'_intercept'), '0.5quant']
-  sd <- temp$sd + RandomComponents$yearTrend[['sd']][i] + FixedList[[i]]$sd + FixedComponents[paste0(repDataSet,'_intercept'), 'sd']
+  median <- temp$median + 
+    FixedList[[i]]$median + 
+    FixedComponents[paste0(repDataSet,'_intercept'), '0.5quant']
+  sd <- temp$sd + 
+    RandomComponents$yearTrend[['sd']][i] + 
+    FixedList[[i]]$sd + 
+    FixedComponents[paste0(repDataSet,'_intercept'), 'sd']
+  
+  if (yearTrend) {
+    median <- median + 
+      RandomComponents$yearTrend[['0.5quant']][i]
+    sd <- temp$sd + 
+      RandomComponents$yearTrend[['sd']][i]
+  }
   
   names(median) <-  yearsIn[i]
   names(sd) <- yearsIn[i]
@@ -347,7 +345,6 @@ for (i in seq(1, length(yearsIn))) {
   STRasterMedian <- c(STRasterMedian, median)
   STRasterSD <- c(STRasterSD, sd)
 }
-
 
 
 STRasters <- list(STRasterMedian = STRasterMedian,
@@ -359,3 +356,55 @@ end <- Sys.time()
 timeTaken <- end - start
 print(timeTaken)
 saveRDS(20 * timeTaken, paste0(modelFolder, "/timeTaken.RDS"))
+
+# Create maps of bias fields
+
+if ("presenceOnlyData" %in% names(specData3)) {
+  cat("Creating bias fields \n")
+  
+  if (biasField) {
+    BiasDat <- split(RandomComponents$sharedBias_biasField,
+                     cut(seq_along(RandomComponents$sharedBias_biasField$mean),
+                         length(yearsIn),
+                         labels = FALSE))
+  }
+  BiasEffects <- RandomComponents$Bias__Effects__Comps
+  
+  BiasRasterMedian <- terra::rast()
+  BiasRasterSD <- terra::rast()
+  
+  for (i in seq(1, length(yearsIn))) {
+    
+    
+    temp2 <- cbind(coords, data.frame(as.matrix(st_drop_geometry(predDataBias[[i]])) %*% 
+                                        as.matrix(BiasEffects[BiasEffects$ID %in% c("distance_roads"),c('sd', '0.5quant')])))
+    names(temp2)[names(temp2) == 'X0.5quant'] <- 'median'
+    
+    if (biasField) {
+      BiasField <- fm_evaluate(modelMesh, data.frame(BiasDat[[i]]), loc = predData[[1]])
+      temp1 <- cbind(coords, data.frame(BiasField[, c('sd' , 'X0.5quant')]))
+      names(temp1)[names(temp1) == 'X0.5quant'] <- 'median'
+      temp <- temp1
+      temp$median <- temp1$median + temp2$median
+      temp$sd <- temp1$sd + temp2$sd
+      temp <- rast(temp)
+    } else {
+      temp <- rast(temp2)
+    }
+    
+    crs(temp) <- fm_wkt(meshToUse)
+    
+    names(temp) <- rep(as.character(yearsIn[i]), 2)
+    temp <- mask(temp, st_sf(regionGeometryProj))
+    BiasRasterMedian <- c(BiasRasterMedian, temp[[2]])
+    BiasRasterSD <-  c(BiasRasterSD, temp[[1]])
+    
+  }
+  BiasRasters <- list(BiasRasterMedian = BiasRasterMedian,
+                      BiasRasterSD = BiasRasterSD)
+  saveRDS(lapply(BiasRasters, terra::wrap), paste0(modelFolder, "/biasFields.RDS"))
+} else {
+  cat("No presence only data, skipping bias field creation\n")
+}
+##Save biasRaster
+

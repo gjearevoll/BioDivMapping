663a9ed08379c355fbc32229d19a36ce7c9766e0
Sam Wenaas Perrin <samp@wallace.win.ntnu.no>
2026-05-29 11:30:01 +0000

    Add model analysis

--- pipeline/parallelModelRun/modelAnalysis.R
+++ pipeline/parallelModelRun/modelAnalysis.R
@@ -14,25 +14,38 @@ if (length(args) != 0) {
   dateAccessed <- args[1]
   # Set the working directory
 }
-#dateAccessed <- "2026-02-09"
+#dateAccessed <- "2026-04-18"
 
 # Set output folders
 folderName <- paste0("data/run_", dateAccessed)
 modelFolderName <- paste0(folderName, "/modelOutputs")
 
+
+# import project control parameters into the environment
+readRDS(paste0(folderName,"/controlPars.RDS")) %>% 
+  list2env(envir = .GlobalEnv)
+
+
 # Get taxa surveyed
 focalTaxa <- read.csv(paste0(folderName, "/focalTaxa.csv"), header = T)
 taxaToCompile <- unique(focalTaxa$taxa)
 
-# Get list of valid models
+# Load segment list
+segmentList <- readRDS(file.path(folderName, "segmentList.RDS"))
+
+# Get list of valid models and other files
 modelNameList <- list.files(modelFolderName, full.names = TRUE, recursive = TRUE, pattern = "richnessModel.qs")
+timeTakenFiles <- list.files(modelFolderName, "timeTaken", recursive = T, full.names = T)
 
 # Get valid model names
 taxaLists <- lapply(taxaToCompile[1], FUN = function(x) {
   modelNameListTaxa <- grep(paste0("/", x), modelNameList, value = TRUE)
   
+  # modelsRun
+  relevantWorkflows <- grep(x, segmentList, value = T)
+  
   # Get predictor species
-  predictorSpecies <- focalTaxa$predictorSpecies[focalTaxa$taxa == x]
+  predictorSpecies <- focalTaxa$predictorSpecies[grep(x, focalTaxa$taxa)]
   
   # Import model for json use
   richnessModel <-  qread(modelNameListTaxa[1])
@@ -100,10 +113,10 @@ taxaLists <- lapply(taxaToCompile[1], FUN = function(x) {
   # Isolate model formulas
   modelFormulasForUse <-  names(richnessModel$bru_info$lhoods)[grep(predictorSpecies, names(richnessModel$bru_info$lhoods))]
   modelFormulas <-  sapply(richnessModel$bru_info$lhoods[modelFormulasForUse],
-                         FUN = function(x) {update.formula(x$formula,
-                                                    new = formula(paste('. ~',
-                                                                        paste0(x$used$effect,
-                                                                               collapse = ' + '))))})
+                           FUN = function(x) {update.formula(x$formula,
+                                                             new = formula(paste('. ~',
+                                                                                 paste0(x$used$effect,
+                                                                                        collapse = ' + '))))})
   modelFormulas <- lapply(modelFormulas, FUN = function(x3) {gsub(paste0(predictorSpecies,"_"), "", x3[3])})
   names(modelFormulas) <- sub("_.*", "", names(modelFormulas))
   
@@ -112,20 +125,21 @@ taxaLists <- lapply(taxaToCompile[1], FUN = function(x) {
     
     #Information about the model
     modelInformation = list(
-      modelFramework = 'Point process',
-      modelType = 'Integrated species distribution model',
-      modelMethod = 'Integrated nested Laplace approximation',
-      statisticalMethodology = 'Bayesian',
-      RVersion = R.version.string,
+      model_framework = 'Point process',
+      model_type = 'Integrated species distribution model',
+      model_method = 'Integrated nested Laplace approximation',
+      statistical_methodology = 'Bayesian',
+      programming_language = 'R',
+      language_version = R.version.string,
       packages = list(packages = c('INLA', 'inlabru','PointedSDMs', 'intSDM'),
                       packageCitations = c(INLA = unlist(citation('INLA')$doi)[1],
                                            inlabru = unlist(citation('inlabru')$doi)[1],
                                            PointedSDMs = unlist(citation('PointedSDMs')$doi)[1],
                                            intSDM = unlist(citation('intSDM')$doi)[1]),
-                      packageVersions = c(INLA = packageVersion('INLA'),
-                                          inlabru = packageVersion('inlabru'),
-                                          PointedSDMs = packageVersion('PointedSDMs'),
-                                          intSDM = packageVersion('intSDM')))
+                      packageVersions = c(INLA = as.character(packageVersion('INLA')),
+                                          inlabru = as.character(packageVersion('inlabru')),
+                                          PointedSDMs = as.character(packageVersion('PointedSDMs')),
+                                          intSDM = as.character(packageVersion('intSDM'))))
       
     )
     ,
@@ -136,10 +150,52 @@ taxaLists <- lapply(taxaToCompile[1], FUN = function(x) {
     )
   )
   
-  returnedData <- list(effects = effectsList, modelData = speciesResponseDataTotal, json = json_taxa)
+  # Produce model diagnostics
+  
+  timeTakenRelevant <- unlist(lapply(timeTakenFiles[grep(x ,timeTakenFiles)], FUN = function(z) {
+    ttFile <- readRDS(z)
+    if (units(ttFile) == "days"){
+      return(as.double(ttFile) * 24)
+    } else {
+      return(as.double(ttFile))}
+  }
+  ))
+  json_model_diagnostics <- list(
+      n_models_run = length(modelNameListTaxa),
+      n_models_attempted = length(relevantWorkflows),
+      n_species_run = length(unique(effectsList$fixedEffects$species)),
+      average_time_run = mean(timeTakenRelevant)
+    )
+  
+  
+  
+  returnedData <- list(effects = effectsList, modelData = speciesResponseDataTotal, json = json_taxa, diagnostics = json_model_diagnostics)
   return(returnedData)
   
 })
 names(taxaLists) <- taxaToCompile[1]
 
-saveRDS(taxaLists, paste0(modelFolderName, "/covAnalysis.RDS"))
+# Compile and saved data objects
+
+# Effects lists first
+effectsLists <- lapply(taxaLists, FUN = function(x) x$effects) |> setNames(taxaToCompile[1])
+saveRDS(effectsLists, paste0(modelFolderName, "/covAnalysis.RDS"))
+
+# Model data object
+modelDataFull <- do.call(rbind, lapply(taxaLists, FUN = function(x) x$modelData))
+write_sf(modelDataFull, file.path(extFolderName, "speciesDataModelled.gpkg"))
+
+# Now ssave metadata - read existing json
+json_ls <- jsonlite::fromJSON(file.path(extFolderName, "metadata.json"))
+
+# Now model diagnostics
+json_ls$step_3a <- lapply(taxaLists, FUN = function(x) x$diagnostics) |> setNames(taxaToCompile[1])
+
+# Model description
+json_ls$step_3b <- lapply(taxaLists, FUN = function(x) x$json) |> setNames(taxaToCompile[1])
+
+# And now write json
+# write json
+jsonlite:::write_json(json_ls,
+                      file.path(extFolderName, "metadata.json"), 
+                      pretty = TRUE)

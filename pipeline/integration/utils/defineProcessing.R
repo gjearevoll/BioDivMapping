cfb765f60e9768d858db64631ca1957cee0b6262
Sam Wenaas Perrin <samp@wallace.win.ntnu.no>
2026-06-08 11:23:06 +0000

    Add in new processing scripts, earlier endpoint definition

--- pipeline/integration/utils/defineProcessing.R
+++ pipeline/integration/utils/defineProcessing.R
@@ -5,6 +5,7 @@
 # reside in functions and begin with "process".
 
 # 0. Check whether a pre-processed version of this dataset exists
+focalEndpoint <- unique(focalData$DWCEndpoint)
 dataFileName <- paste0(tempFolderName,"/", datasetName ,"/processedDataset.RDS")
 if (file.exists(dataFileName)) {
   cat("\tPre-processed version used\n")
@@ -12,60 +13,64 @@ if (file.exists(dataFileName)) {
   
   # 1. The national insect Monitoring in Norway dataset
 } else if (dataType == "insectMonitoring") {
-  focalEndpoint <- unique(focalData$DWCEndpoint)
   newDataset <- processNationalInsectMonitoring(focalData, focalEndpoint, tempFolderName, crs, coordUncertainty)
   
   # 1b. The national insect Monitoring in Norway dataset sturcture, but for other similar datasets
 } else if (dataType == "insectMonitoringStandard") {
-  focalEndpoint <- metadata$DWCEndpoint[metadata$name == datasetName]
   newDataset <- processInsectMonitoring(focalData, focalEndpoint, tempFolderName, crs, coordUncertainty)
   
   # 2. ANO Data
-} else if (dataType == "ANO") {
-  newDataset <- processANOData(focalData, crs)
+} else if (dataType == "ANOData") {
+  newDataset <- processANOData(focalEndpoint, tempFolderName, datasetName, regionGeometry, focalTaxon, crs, yearToStart) 
   
   # 3. Field note data  
 } else if (dataType == "fieldNotes"){
-  focalEndpoint <- metadata$DWCEndpoint[metadata$name == datasetName]
   newDataset <- processFieldNotes(focalEndpoint, tempFolderName, datasetName, regionGeometry, focalTaxon, crs, coordUncertainty, yearToStart)
   
   # 4. Field note data  - Oslo and Agder
 } else if (dataType == "fieldNotesOslo"){
-  focalEndpoint <- metadata$DWCEndpoint[metadata$name == datasetName]
   newDataset <- processFieldNotesOslo(focalEndpoint, tempFolderName, datasetName, regionGeometry, focalTaxon, crs, 
                                       coordUncertainty, yearToStart)
   
   
   # 5. Field note data (with events table)
 } else if (dataType == "fieldNotesEvent") {
-  focalEndpoint <- unique(focalData$DWCEndpoint)
   newDataset <- processFieldNotesEvent(focalEndpoint, tempFolderName, datasetName, regionGeometry, focalTaxon, crs, coordUncertainty, yearToStart)
   
-  # No need to do anything to presence only data (yet) except add individualCount column
+  # 6. NTNU freshwater collection
+} else if (dataType == "NTNUFreshwater") {
+  newDataset <- processNTNUFreshwater(focalEndpoint, tempFolderName, datasetName, regionGeometry, focalTaxon, crs, coordUncertainty, yearToStart)
+  
+  # 7. Freshwater fish inventory Norway
 } else if (dataType == "freshwaterFishInventory") {
-  focalEndpoint <- metadata$DWCEndpoint[metadata$name == datasetName]
   newDataset <- processFreshwaterFishInventory(focalEndpoint, tempFolderName, datasetName, regionGeometry, focalTaxon, crs, coordUncertainty, yearToStart)
   
   # No need to do anything to presence only data (yet) except add individualCount column
 }else if (dataType == "mareano") {
-  focalEndpoint <- metadata$DWCEndpoint[metadata$name == datasetName]
   newDataset <- processMareano(focalEndpoint, tempFolderName, datasetName, regionGeometry, focalTaxon, crs)
   
   # No need to do anything to presence only data (yet) except add individualCount column
 } else if (dataType == "marine1") {
-  focalEndpoint <- metadata$DWCEndpoint[metadata$name == datasetName]
   newDataset <- processMarine1(focalEndpoint, tempFolderName, datasetName, regionGeometry, focalTaxon, crs)
   
   # No need to do anything to presence only data (yet) except add individualCount column
 } else if (dataType == "marine2") {
-  focalEndpoint <- metadata$DWCEndpoint[metadata$name == datasetName]
   newDataset <- processMarine2(focalEndpoint, tempFolderName, datasetName, regionGeometry, focalTaxon, crs)
   
+  # No need to do anything to presence only data (yet) except add individualCount column
+}else if (dataType == "vannmiljo") {
+  newDataset <- processVannmiljo(focalEndpoint, tempFolderName, datasetName, regionGeometry, focalTaxon, crs, coordUncertainty, yearToStart)
+
   # No need to do anything to presence only data (yet) except add individualCount column
 }else if (dataType == "presenceOnly") {
   focalData$dataType <- "PO"
   newDataset <- focalData[,c("acceptedScientificName", "geometry", "dataType", "taxa", "year", "taxonKeyProject", "redListStatus")]
   newDataset <- st_transform(newDataset, crs)
+  
+} else if (dataType == "citizenScience") {
+  focalData$dataType <- "PO"
+  newDataset <- processCitizenScience(focalData, regionGeometry, '+proj=utm +zone=33 +datum=WGS84 +units=km +no_defs', tempFolderName, datasetName)
+  
 } else {
   focalData$dataType <- "PO"
   newDataset <- focalData[,c("acceptedScientificName", "geometry", "dataType", "taxa", "year", "taxonKeyProject")]
@@ -85,7 +90,7 @@ if ("data.frame" %in% class(newDataset)) {
     newDataset$year <- sapply(newDataset$year, function(x) {x + max(negative_number(yearInterval - x))})
   }
   
-  if (dataType %in% c("presenceOnly", "PO")) {
+  if (dataType %in% c("presenceOnly", "PO", "citizenScience")) {
     arrangedData <- newDataset %>%
       arrange(-year)
   } else {
